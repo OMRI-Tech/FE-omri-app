161abab0bc198f507d4b2d16a15a1002514312cd
GandyA23 <avila.gandy@gmail.com>
2022-07-13 17:56:30 -0500

    Add: Buttons in Quiz template
    Upload progress in issue.
    Add new styles for 4 buttons, in normal state and selected state.
    Add buttons in template.

--- src/pages/Quiz.vue
+++ src/pages/Quiz.vue
@@ -1,9 +1,21 @@
 <template>
   <q-page>
-    <!-- Div with card background -->
+    <!-- Div with cards background -->
     <div class="question-bg column justify-center">
       <div class="question">
       </div>
+    </div>
+
+    <!-- Asnwers -->
+    <div class="answers">
+      <q-btn
+        v-for="(btn, index) in buttons"
+        :key="`btn-option-${(index+1)}`"
+        size="lg"
+        :class="`full-width answ option-${(index+1)}${buttons[index] ? '-selected' : ''}`"
+        label="Lorem ipsum dolor"
+        @click="selected(index)"
+        />
     </div>
   </q-page>
 </template>
@@ -13,5 +25,19 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Quiz",
+  data () {
+    return {
+      buttons: [false, false, false, false]
+    }
+  },
+  methods: {
+    /**
+     * Change value in buttons array to change the class
+     * If you use more than four buttons, add new classes in quiz.sass file
+     */
+    selected (option) {
+      this.buttons[option] = !this.buttons[option]
+    }
+  }
 });
 </script>
