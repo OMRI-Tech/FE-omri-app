5c9914ed0774c5108fd633a1e9e4a5747fa4bad7
GandyA23 <avila.gandy@gmail.com>
2022-07-13 16:00:22 -0500

    Add: Cards in Quiz template
    Upload progress in issue.
    Add a new asset for the background in questions, a svg for better quality than a PNG.
    Draw elements to show the question.

---
+++ src/layouts/QuizLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout>
+    <q-header class="header">
+      <q-toolbar class="justify-center">
+        Multiple Questions
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+import "src/css/quiz.sass"
+
+export default defineComponent({
+  name: "QuizLayout",
+});
+</script>
---
+++ src/pages/Quiz.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <!-- Div with card background -->
+    <div class="question-bg column justify-center">
+      <div class="question">
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Quiz",
+});
+</script>
