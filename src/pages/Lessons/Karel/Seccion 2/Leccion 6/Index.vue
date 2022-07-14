cf39bc618721f6fe7769de9972ed922b547a891d
JoseMM2002 <jeolmmt@gmail.com>
2022-07-14 14:58:39 -0500

    Karek seccion 2 leccion 6

---
+++ src/pages/Lessons/Karel/Seccion 2/Leccion 6/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h4>Esta es la sección 2</h4>
+    <h5>lección 6</h5>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+export default defineComponent({
+  name: 'ks26',
+  setup () {
+    const images = {
+        img1: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image001.png'),
+        img2: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image002.png'),
+        img3: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image003.png'),
+        img4: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image004.png'),
+        img5: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image005.png'),
+        img6: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image006.png'),
+        img7: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image007.png')
+    }
+  }
+})
+</script>
