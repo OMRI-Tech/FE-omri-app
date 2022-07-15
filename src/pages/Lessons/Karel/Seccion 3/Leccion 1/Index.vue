688901389c5effe467937b109f803eb4e81cbc23
JoseMM2002 <jeolmmt@gmail.com>
2022-07-14 19:44:24 -0500

    Karel Seccion 3 Leccion 1

---
+++ src/pages/Lessons/Karel/Seccion 3/Leccion 1/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h4>Esta es la sección 2</h4>
+    <h5>lección 7</h5>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+export default defineComponent({
+  name: 'ks26',
+  setup () {
+    const images = {
+        img1: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image001.png'),
+        img2: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image002.png'),
+        img3: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image003.png'),
+        img4: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image004.png'),
+        img5: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image005.png'),
+        img6: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image006.png'),
+        img7: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image007.png'),
+        img8: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image008.png'),
+        img9: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image009.png'),
+        img9_2: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image009_2.png'),
+        img10: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image010.png'),
+        img11: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image011.png'),
+        img12: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image012.png'),
+        img13: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image013.png'),
+        img14: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image014.png'),
+        img15: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image015.png'),
+        img16: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image016.png'),
+        img16_2: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image016_2.png'),
+        img17: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image017.png'),
+        img18: require('src/pages/Lessons/Karel/Seccion 3/Leccion 1/Images/image018.png'),
+    }
+  }
+})
+</script>
