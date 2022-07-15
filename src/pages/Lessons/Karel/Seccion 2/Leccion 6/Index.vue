a8f518f5f8cd27041f554723c5a6db5f932f6865
JoseMM2002 <jeolmmt@gmail.com>
2022-07-14 19:21:31 -0500

    mejorar orden

--- src/pages/Lessons/Karel/Seccion 2/Leccion 6/Index.vue
+++ src/pages/Lessons/Karel/Seccion 2/Leccion 6/Index.vue
@@ -11,13 +11,13 @@ export default defineComponent({
   name: 'ks26',
   setup () {
     const images = {
-        img1: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image001.png'),
-        img2: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image002.png'),
-        img3: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image003.png'),
-        img4: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image004.png'),
-        img5: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image005.png'),
-        img6: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image006.png'),
-        img7: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/image007.png')
+        img1: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image001.png'),
+        img2: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image002.png'),
+        img3: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image003.png'),
+        img4: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image004.png'),
+        img5: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image005.png'),
+        img6: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image006.png'),
+        img7: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image007.png')
     }
   }
 })
