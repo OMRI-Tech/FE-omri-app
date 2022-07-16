be71bc087d3ce9ed2beaff1e147fcb6bff54ab63
JoseMM2002 <jeolmmt@gmail.com>
2022-07-16 15:10:43 -0500

    cambio a karousel ks2l6

--- src/pages/Lessons/Karel/Seccion 2/Leccion 6/Index.vue
+++ src/pages/Lessons/Karel/Seccion 2/Leccion 6/Index.vue
@@ -1,29 +1,52 @@
 <template>
   <div class="items-center div-padding">
-    <div v-for="(image,index) in images" :key="index" class="text-center bg-white">
-      <img :src="image" class="img-content">
-    </div>
+    <q-carousel 
+      animated 
+      v-model="slide"
+      arrows
+      class="carousel-content"
+      control-color="primary"
+      v-model:fullscreen="fullscreen"
+      >
+      <q-carousel-slide v-for="image in images" :key="image.id" class="text-center bg-white" :name="image.id">
+        <q-img :src="image.img"/>
+      </q-carousel-slide>
+      <template v-slot:control>
+        <q-carousel-control
+          position="bottom-right"
+          :offset="[18, 18]"
+        >
+          <q-btn
+            push round dense color="primary" text-color="white"
+            :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
+            @click="fullscreen = !fullscreen"
+          />
+        </q-carousel-control>
+      </template>
+    </q-carousel>
   </div>
 </template>
 
 <script>
-import { defineComponent, onBeforeMount } from "vue"
+import { defineComponent, onBeforeMount, ref } from "vue"
 export default defineComponent({
   name: 'ks26',
   setup ( props, {emit}) {
     onBeforeMount(() => {
       emit('title','Introducción a Karel')
     })
-    const images = {
-        img1: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image001.png'),
-        img2: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image002.png'),
-        img3: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image003.png'),
-        img4: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image004.png'),
-        img5: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image005.png'),
-        img6: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image006.png'),
-        img7: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image007.png')
-    }
-    return { images } 
+    const images = [
+        {img: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image001.png'), id: 1},
+        {img: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image002.png'), id: 2},
+        {img: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image003.png'), id: 3},
+        {img: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image004.png'), id: 4},
+        {img: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image005.png'), id: 5},
+        {img: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image006.png'), id: 6},
+        {img: require('src/pages/Lessons/Karel/Seccion 2/Leccion 6/Images/image007.png'), id: 7}
+    ]
+    const slide = ref(1)
+    const fullscreen = ref(false)
+    return { images, slide, fullscreen} 
   }
 })
 </script>
