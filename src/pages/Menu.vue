56e0b8e4f781d7438ba0727c0fad1b50c90fa568
pronficilio <isailandao@hotmail.com>
2022-11-15 14:23:07 -0600

    Se agregan dialog de perfil incompleto, visualizador de video, se desbloquean niveles

--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -5,18 +5,19 @@
         <p class="txt-title">Bienvenido {{ user.name }} </p>
         <p class="txt-progress">Progreso</p>
         <q-linear-progress rounded :value="0.3" size="8px" class="q-my-sm" />
-        <p class="txt-level">Intermedio {{olimpico}}a</p>
+        <p class="txt-level">Categoría: {{ user.posible_paquete.paquete.nombre_paquete }}</p>
       </div>
       <div class="info-image column inline justify-end">
         <q-img class="q-mx-auto squares" :src="images.squares" spinner-color="white" />
         <q-img class="q-mx-auto entrenator" :src="images.entrenator" spinner-color="white" />
       </div>
     </div>
+
     <div class="main-container">
       <div class="entrenator-background vector">
         <div class="info">
-          <p>Nivel actual</p>
-          <p>NIVEL 6</p>
+          <p>{{user.nivel !== null ? user.nivel.name : '--'}}</p>
+          <p>NIVEL {{user.id_nivel}}</p>
         </div>
       </div>
       <div class="vector-3 vector" :style="desplazamiento(3)"></div>
@@ -26,30 +27,124 @@
       <div class="path vector">
       </div>
       <div class="contenedor-figura vector">
-        <div class="figura triangle done">1</div>
-        <div class="figura circle done">2</div>
-        <div class="figura hexa visited">3</div>
-        <div class="figura hexa">4</div>
-        <div class="figura star">5</div>
-        <div class="figura star">6</div>
-        <div class="figura hexa">7</div>
-        <div class="figura triangle">8</div>
-        <div class="figura circle">9</div>
+
+        <div v-for="(step, i) in levels" :key="i" :class="'figura ' + step.icon + ' ' + step.status">
+          <div v-if="step.url !== undefined && step.status != ''" @click="changeUrlVideo(step)">
+            {{ step.step }}
+          </div>
+          <div v-else>
+            {{step.step}}
+          </div>
+        </div>
       </div>
     </div>
+    <q-dialog v-model="dialogPerfilIncompleto" persistent transition-show="scale" transition-hide="scale">
+      <q-card class="text-white main-rectangle" style="width: 500px">
+        <q-card-section>
+          <div class="text-h5">Perfil incompleto</div>
+        </q-card-section>
+        <q-card-section class="q-pt-none" style="font-size: 20px">
+          Completa tu perfil para continuar
+        </q-card-section>
+
+        <q-card-actions align="center" class="bg-whte text-teal">
+          <q-btn color="accent" :to="{ name: 'Profile' }" label="Click aqui para llenar perfil" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-dialog
+      v-model="dialogVideo"
+      persistent
+      :maximized="maximizedToggle"
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card class="text-white" style="background: #003F54">
+        <q-bar>
+          <q-space />
+
+          <q-btn dense flat icon="minimize" @click="maximizedToggle = false" :disable="!maximizedToggle">
+            <q-tooltip v-if="maximizedToggle" class="bg-white text-primary">Minimizar</q-tooltip>
+          </q-btn>
+          <q-btn dense flat icon="crop_square" @click="maximizedToggle = true" :disable="maximizedToggle">
+            <q-tooltip v-if="!maximizedToggle" class="bg-white text-primary">Maximizar</q-tooltip>
+          </q-btn>
+          <q-btn dense flat icon="close" v-close-popup>
+            <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+
+        <q-card-section>
+          <div class="text-h6">{{tituloVideo}}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none text-center">
+          <q-video
+            :ratio="16/9"
+            ref="player"
+            v-bind:src='urlVideo'
+            frameborder="0"
+            id="videito"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;"
+            :title="tituloVideo"
+            allowfullscreen />
+          <q-btn color="accent" label="Click aqui para marcar como visto" @click="marcaProgreso()" v-close-popup />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
 <script>
 import { defineComponent, onMounted, ref, computed } from 'vue'
 import { useStore } from 'vuex'
-
+import { Loading, QSpinnerBall } from 'quasar'
 export default defineComponent({
   name: 'MainMenu',
-  setup () {
+  setup (props, context) {
     const store = useStore()
-    const user = computed(() => store.getters['auth/user'])
-    let olimpico = computed(() => store.state.auth.olimpico)
+    const user = store.getters['auth/user']
+    var niveles = store.getters['auth/dameNiveles']
+    var progreso = store.getters['auth/dameProgreso']
+    const dialogVideo = ref(false)
+    const levels = ref(niveles[user.id_nivel - 1] !== undefined && progreso !== null ? niveles[user.id_nivel - 1].map((v, i) => {
+      let progress = progreso.filter(p => p.step == v.step)
+      return {...v, status: progress.length ? (progress[0].realizado == 0 ? 'done' : 'visited') : '' }
+    }) : null);
+
+    const urlVideo = ref('')
+    const tituloVideo = ref('')
+    const stepActual = ref(null)
+    const changeUrlVideo = (step) => {
+      Loading.show({ message: 'Cargando video', spinner: QSpinnerBall })
+      urlVideo.value = step.url
+      tituloVideo.value = step.title
+      stepActual.value = step
+      dialogVideo.value = true
+      setTimeout(()=>{
+        Loading.hide()
+      }, 3500)
+    }
+    const marcaProgreso = () => {
+      console.log('---------- se empieza a marcar progreso')
+      store.dispatch('auth/marcaProgreso', {
+        step: stepActual.value.step,
+        nivel_id: user.id_nivel,
+        user_id: user.id
+      }).finally(() => {
+          setTimeout(()=>{
+            console.log('----------- tenemos por aqui el then')
+            progreso = store.getters['auth/dameProgreso']
+            context.emit('actualizaVidas')
+            levels.value = niveles[user.id_nivel - 1].map((v, i) => {
+              let progress = progreso.filter(p => p.step == v.step)
+              return {...v, status: progress.length ? (progress[0].realizado == 0 ? 'done' : 'visited') : '' }
+            })
+            console.log('ok se actualizó los levels ', progreso, levels.value)
+          }, 3500)
+        }
+      )
+    }
     const images = {
       squares: require('assets/img/squares.png'),
       entrenator: require('assets/img/entrenator.png')
@@ -64,6 +159,8 @@ export default defineComponent({
         min: -400, max: -100, actual: -250, dir: 'der'
       }
     ])
+    // dialog solicitando completar perfil
+    const dialogPerfilIncompleto = ref(false)
     const desplazamiento = ((i) => {
       return {
         'background-position-x': direccion.value[i].actual + "px"
@@ -87,19 +184,30 @@ export default defineComponent({
       }
     }
     onMounted(()=>{
+      if(user.grado === null){
+        dialogPerfilIncompleto.value = true
+      }
       setInterval(animameEsta, 6500)
     })
-
+    console.log(user)
     return {
       direccion,
       images,
       desplazamiento,
       user,
-      olimpico
+      dialogPerfilIncompleto,
+      levels,
+      urlVideo,
+      changeUrlVideo,
+      tituloVideo,
+      stepActual,
+      dialogVideo,
+      maximizedToggle: ref(true),
+      marcaProgreso
     }
   },
   mounted () {
-    this.$emit('activeTitle','')
+    this.$emit('activeTitle', 'Menú')
   }
 })
 </script>
