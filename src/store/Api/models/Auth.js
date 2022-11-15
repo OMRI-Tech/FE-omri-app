fc9bd042de401948af942eb8e982a671677b82ac
pronficilio <isailandao@hotmail.com>
2022-11-15 14:15:26 -0600

    Se agregan endpoints

--- src/store/Api/models/Auth.js
+++ src/store/Api/models/Auth.js
@@ -11,6 +11,14 @@
     updatePass: 'api/editaContra/',
     agregarInscripcion: 'api/aplica-colegiatura',
     payLink: 'api/conekta/link',
-    login:  `api/${model}/login`
+    rifaLoca: 'api/guarda-boleto-rifa',
+    encuentraAlumno: 'api/encuentra-alumnos-por-telefono',
+    otpLogin: 'api/otp-login/',
+    selectEscuela: 'api/select2-escuela',
+    selectMunicipio: 'api/select2-municipio',
+    vidas: 'api/get-vida/',
+    login:  `api/${model}/login`,
+    dameProgreso: 'api/dame-mi-progreso',
+    marcaProgreso: 'api/marca-como-realizado'
   }
 }
