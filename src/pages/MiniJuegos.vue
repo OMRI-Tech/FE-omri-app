ebb4bbb57ef2e004e02b2fcf02d2b1823386ec3b
JoseMM2002 <jeolmmt@gmail.com>
2022-12-28 23:38:46 -0600

    generador de colores

--- src/pages/MiniJuegos.vue
+++ src/pages/MiniJuegos.vue
@@ -1,19 +1,58 @@
 <template>
     <div class="text-h6">
-        AMARILLO
+        <div v-for="(linea, index) in mezclados" :key="index" class="row">
+            <div 
+            v-for="(item, index) in linea" 
+            @click="clickColor(item)" 
+            :key="index" 
+            :class="'col text-center q-pa-md text-' + item.color"
+            >
+                {{ item.label }}
+                <q-separator/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { reactive } from '@vue/reactivity'
 export default {
     setup () {
-        
+        let random = Math.random()
+        const colores = [
+            { label: 'Amarillo', color: 'yellow' },
+            { label: 'Verde', color: 'green' },
+            { label: 'Azul', color: 'blue' },
+            { label: 'Rosa', color: 'pink' },
+            { label: 'Rojo', color: 'red' },
+            { label: 'Gris', color: 'grey' },
+            { label: 'Cafe', color: 'brown' },
+            { label: 'Morado', color: 'purple'},
+            { label: 'Naranja', color: 'orange'},
 
-        return {}
+        ]
+        const mezclados = reactive([])
+        for (let i = 0; i < 6; i++) {
+            let arr = []
+            for (let j = 0; j < 3; j++) {
+                let aux1 = Math.floor(random * 9)
+                random = (random * 10) - (Math.floor(random * 10))
+                let aux2 = Math.floor(random * 9)
+                random = (random * 10) - (Math.floor(random * 10))
+                arr.push({label: colores[aux1].label, color: colores[aux2].color})
+            }
+            mezclados.push(arr);
+        }
+        const clickColor = (item) => {
+            console.log(item)
+        }
+        return {
+            mezclados, 
+            clickColor
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
