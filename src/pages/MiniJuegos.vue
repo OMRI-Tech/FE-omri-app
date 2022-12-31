737c095b8a045ea6ff20e3a8cbcfaff9cecb636f
JoseMM2002 <jeolmmt@gmail.com>
2022-12-30 21:57:15 -0600

    MaxScore, dialogo final

--- src/pages/MiniJuegos.vue
+++ src/pages/MiniJuegos.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="text-subtitle2">
-        <div class="text-center row q-pa-xs">
-            <div class="col">
-            </div>
-            <div class="col-auto-md">
+        <div class="text-center row q-pa-sm">
+            <div class="col-8">
                 Selecciona por color
                 <div class="text-italic">
                     {{color.label}}
@@ -38,24 +36,37 @@
                 <q-separator/>
             </div>
         </div>
-        <q-dialog v-model="ModelWinner">
+        <q-dialog v-model="ModelDialog" persistent>
             <q-card class="absolute-center text-center q-pa-lg">
-                Ganaste!
+                {{dialog}}
+                <br>
+                Obtuviste {{score}} puntos
+                <br>
+                Tu máximo es {{maxScore}}
+                <q-btn flat color="teal q-mt-md" @click="volverMenu()">Menu</q-btn>
+                <q-btn flat color="teal q-mt-md" @click="volverAJugar()">
+                    <q-icon name="refresh" color="teal"></q-icon>
+                </q-btn>
             </q-card>
         </q-dialog>
     </div>
 </template>
 
 <script>
+
 import { reactive, ref } from '@vue/reactivity'
 import { onBeforeMount } from '@vue/runtime-core'
-import router from 'src/router'
+import { useRouter } from 'vue-router'
+
 export default {
     setup () {
+        const router = useRouter()
         let random = Math.random()
         const score = ref(0)
+        const maxScore = ref(0)
         const timeMounted = ref(new Date(Date.now()))
-        const ModelWinner = ref(false)
+        const ModelDialog = ref(false)
+        const dialog = ref("")
         const colores = reactive([
             { label: 'Amarillo', color: 'yellow', count: 0 },
             { label: 'Verde', color: 'green', count: 0 },
@@ -67,47 +78,78 @@ export default {
             { label: 'Morado', color: 'purple', count: 0},
             { label: 'Naranja', color: 'orange', count: 0},
         ])
-        const color = colores[Math.floor(random * 9)]
+        const color = ref(colores[Math.floor(random * 9)])
         random = Math.random()
         const mezclados = reactive([])
         const count = ref(0)
-        for (let i = 0; i < 9; i++) {
-            let arr = []
-            for (let j = 0; j < 3; j++) {
-                let aux1 = Math.floor(random * 9)
-                random = Math.random()
-                let aux2 = Math.floor(random * 9)
-                random = Math.random()
-                colores[aux2].count++
-                arr.push({
-                    label: colores[aux1].label, 
-                    color: colores[aux2].color, 
-                    checked: false, 
-                    isTheColor: (colores[aux2].color == color.color)
-                })
-                count.value += (colores[aux2].color == color.color)
+        const mezclarColores = () => {
+            for (let i = 0; i < 9; i++) {
+                let arr = []
+                for (let j = 0; j < 3; j++) {
+                    let aux1 = Math.floor(random * 9)
+                    random = Math.random()
+                    let aux2 = Math.floor(random * 9)
+                    random = Math.random()
+                    colores[aux2].count++
+                    arr.push({
+                        label: colores[aux1].label, 
+                        color: colores[aux2].color, 
+                        checked: false, 
+                        isTheColor: (colores[aux2].color == color.value.color)
+                    })
+                    count.value += (colores[aux2].color == color.value.color)
+                }
+                mezclados.push(arr);
             }
-            mezclados.push(arr);
+        }
+        const limpiarColores = () =>{
+            for(let i = 0; i < 9; i++) {
+                mezclados.pop()
+            }
         }
         const clickColor = (item) => {
-            score.value += Math.floor((item.isTheColor && !item.checked) * (120 * timer.value /20))
-            count.value -= (item.isTheColor && !item.checked)
-            item.checked = true
-            ModelWinner.value = (count.value == 0)
+            if (timer != 0){
+                score.value += Math.floor((item.isTheColor && !item.checked) * (120 * timer.value /20))
+                score.value -= (!item.isTheColor && !item.checked) * 35
+                count.value -= (item.isTheColor && !item.checked)
+                item.checked = true
+                ModelDialog.value = (count.value == 0)
+                if (ModelDialog.value){
+                    dialog.value = "Ganaste!"
+                    maxScore.value = score.value > maxScore.value ? score.value : maxScore.value
+                }
+            }
         }
         const timerValue = () => {
             const now = new Date(Date.now())
             return 20 - (now - timeMounted.value)/1000
         }
         const timer = ref(0)
+        const volverMenu = () => {
+            router.push({ name: 'Home' })
+        }
+        const volverAJugar = () => {
+            color.value = colores[Math.floor(random * 9)]
+            random = Math.random()
+            do {
+                limpiarColores()
+                mezclarColores()
+            } while (count.value == 0);
+            ModelDialog.value = false
+            timeMounted.value = new Date(Date.now())
+            score.value = 0
+        }
         setInterval(() => {
-            timer.value = timerValue() > 0 ? timerValue() : 0
+            timer.value = (timerValue() > 0 && count.value > 0) ? timerValue() : 0
+            if (timer.value == 0 && count.value > 0){
+                ModelDialog.value = true
+                dialog.value = "Fallaste por muy poco :("
+                maxScore.value = score.value > maxScore.value ? score.value : maxScore.value
+            }
         }, 250)
         onBeforeMount(() => {
-            console.log(count.value)
-            if(!(count.value > 0)){
-                console.log("hice otro render")
-                router.push({name:"MiniGames"})
+            while(count.value == 0){
+                mezclarColores()
             }
         })
         return {
@@ -115,9 +157,13 @@ export default {
             clickColor,
             color,
             score,
+            maxScore,
             timeMounted,
             timer,
-            ModelWinner
+            ModelDialog,
+            dialog,
+            volverMenu,
+            volverAJugar
         }
     }
 }
